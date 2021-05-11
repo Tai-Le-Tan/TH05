cc80025c2d151450bfbf0b272a07a9994b310cb1
Tai-Le-Tan <76083222+Tai-Le-Tan@users.noreply.github.com>
2021-05-11 10:25:35 +0700

    Push Lan2

---
+++ TH05/Controllers/LoginUserController.cs
@@ -0,0 +1,64 @@
+﻿using Antlr.Runtime.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TH05.Models;
+
+namespace TH05.Controllers
+{   
+    public class LoginUserController : Controller
+
+    {
+        DBSportStoreEntities1 database = new DBSportStoreEntities1();
+        // GET: LoginUser
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LoginAcount(AdminUser _user)
+        {
+            var check = database.AdminUsers.Where(s => s.ID == _user.ID && s.PasswordUser == _user.PasswordUser).FirstOrDefault();
+            if (check == null)
+            {
+                ViewBag.ErrorInfo = "SaiInfo";
+                return View("Index");
+            }
+            else
+            {
+                database.Configuration.ValidateOnSaveEnabled = false;
+                Session["ID"] = _user.ID;
+                Session["PasswordUser"] = _user.PasswordUser;
+                return RedirectToAction("Index", "Product");
+            }
+        }
+        public ActionResult RegisterUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RegisterUser(AdminUser _user)
+        {
+            if(ModelState.IsValid)
+            {
+                var check_id = database.AdminUsers.Where(s => s.ID == _user.ID).FirstOrDefault();
+                if(check_id==null)
+                {
+                    database.Configuration.ValidateOnSaveEnabled = false;
+                    database.AdminUsers.Add(_user);
+                    database.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.ErrorRegister = "This ID is exit";
+                    return View();
+                }
+            }
+            return View();
+        }
+    }
+    
+}
